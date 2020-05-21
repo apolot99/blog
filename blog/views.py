cd3bab5533b814cd3767f7be2d34096acc040d12
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-05-21 16:44:37 -0400

    update

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView # new
 
+from django.views.generic.edit import CreateView # new
+
 from .models import Post
+
 
 class BlogListView(ListView):
    
@@ -17,5 +20,13 @@ class BlogDetailView(DetailView): # new
     model = Post
     
     template_name = 'post_detail.html'
+    
+class BlogCreateView(CreateView): # new
+    
+    model = Post
+    
+    template_name = 'post_new.html'
+    
+    fields = ['title', 'author', 'body']
     
     
